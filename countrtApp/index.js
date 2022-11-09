d4ea38f1283fdea1578f34c4c15b7e5ac7e479bf
KACHHAP LILY <lilykumarikachhap@gmail.com>
2022-11-09 21:39:57 +0530

    first commit

---
+++ countrtApp/index.js
@@ -0,0 +1,16 @@
+let counting = document.getElementById("counting");
+let savedata = document.getElementById("savedata");
+let count =0;
+
+function Count(){
+    count =+ count +1;
+    counting.textContent =count;
+}
+
+function save(){
+    let str = count + "-"
+    savedata.textContent += str;
+    counting.textContent =0;
+  
+}
+
