ab7b38690e754b8752f90c7ae2b36f8517c63cb7
KACHHAP LILY <lilykumarikachhap@gmail.com>
2022-12-28 12:38:15 +0530

    Ajax two api object marge

--- Ex-31 Sync two Api/index.js
+++ Ex-31 Sync two Api/index.js
@@ -2,19 +2,18 @@
 const xhttp1 = new XMLHttpRequest();
 const xhttp2 = new XMLHttpRequest();
 let albums = [];
-let userbody = [];
 
 
 xhttp1.onload = function () {
     albums = JSON.parse(this.responseText);
-
 }
 xhttp2.onload = function () {
     let resp = JSON.parse(this.responseText);
-    resp.forEach(element => {
-        userbody.push(element.body)
+    for(let i=0;i<albums.length;i++){
+        albums[i]["Body"]=resp[i].body;
+    }
 
-    });   
+    console.log(albums);
 }
 
 xhttp1.open("GET", "https://jsonplaceholder.typicode.com/albums", false);
@@ -24,16 +23,16 @@ xhttp2.open("GET", " https://jsonplaceholder.typicode.com/posts", false);
 xhttp1.send();
 xhttp2.send();
 
-display=()=>{
-let th="";
+display = () => {
+    let th = "";
     for (const key in albums[0]) {
-       th+= `<th>${key} </th>`
+        th += `<th>${key} </th>`
     }
-    document.getElementById("thead").innerHTML=th;
-    document.getElementById('tbody').innerHTML=albums.map((ele)=>{
-let td="";
+    document.getElementById("thead").innerHTML = th;
+    document.getElementById('tbody').innerHTML = albums.map((ele) => {
+        let td = "";
         for (const key in ele) {
-            td+=`<td>${ele[key]}</td>`          
+            td += `<td>${ele[key]}</td>`
         }
         return `<tr>${td}</tr>`
     }).join(" ")
