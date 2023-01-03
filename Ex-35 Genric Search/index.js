6950977c4f1f7cf3ffd09c3a69918fe91499e729
KACHHAP LILY <lilykumarikachhap@gmail.com>
2023-01-03 12:17:05 +0530

    genric search

--- Ex-35 Genric Search/index.js
+++ Ex-35 Genric Search/index.js
@@ -4,30 +4,40 @@ fetch("https://jsonplaceholder.typicode.com/comments").then(y => y.json()).then
 
 display = (y) => {
     myarray = y;
-    document.getElementById("tbody").innerHTML=y.map((ele,index)=>{
-        let td=" ";
+    document.getElementById("tbody").innerHTML = y.map((ele) => {
+        let td = " ";
         for (const key in ele) {
-            td+=`<td>${ele[key]} </td>`                
+            td += `<td>${ele[key]} </td>`
         }
-        return `<tr id="${index+1}">${td}</tr>`
-        
+        return `<tr id="${ele.id}">${td}</tr>`
+
     }).join(" ")
 
 }
 srchdata = () => {
-    let srchid=document.getElementById('srchid').value;
-   let filtersrch=myarray.filter((ele)=>{
-        return (Object.values(ele)).includes(srchid);
+    let srchid = document.getElementById('srchid').value;
+
+
+    let filtersrch = myarray.filter((ele) => {
+        let text = Object.values(ele).join(" ");
+        return text.indexOf(srchid) >= 0;
+
     })
-    
+  
+console.log(filtersrch);
 
-    let tr=document.getElementsByTagName('tr');
-    for (iterator of tr) {
-        console.log(iterator);
-        if(iterator.id==filtersrch[0].id){
-            document.getElementById(`${iterator.id}`).style.backgroundColor="yellow";
+    let tr = document.getElementsByTagName('tr');
+   
+      for (const iterator of tr) {
+       for (const i of filtersrch) {
+        if(iterator.id==i.id){
+           document.getElementById(`${iterator.id}`).style.backgroundColor="yellow"
         }
-    }
-
+        
+       }
+        
+      }
     
+
+
 }
