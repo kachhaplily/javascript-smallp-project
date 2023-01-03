470cd787edc38e046c54b6d75ee4faaf77b78418
KACHHAP LILY <lilykumarikachhap@gmail.com>
2023-01-03 13:18:37 +0530

    number sorting

---
+++ Ex-36 Genric Array Soring Prototype/index.js
@@ -0,0 +1,20 @@
+let a = [1, 8, 9, 10, 13, 02]
+
+Array.prototype.mysorting = function () {
+    return this.sort((a,b) => {
+        if(a<b){
+            return 1;
+        }else{
+            return -1;
+        }
+
+    })
+}
+console.log(text.mysorting());
+
+
+
+
+
+
+
